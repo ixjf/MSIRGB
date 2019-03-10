59bf818aa77197afaa4a88aed13bfa4dec524d49
Pedro Fanha <ixjf@outlook.com>
2019-03-10 23:58:25 +0000

    Small fixes
    - Increased temperature limits
    - Fully update to work with v2.0.0 (i.e. change colour ranges to 0x0-0xf)
    - Remove 'AIDA64 is not running' print appearing every iteration of the loop (massive log file after a while)
    - Other small changes

--- Scripts/Hue Wheel AIDA64.lua
+++ Scripts/Hue Wheel AIDA64.lua
@@ -1,93 +1,85 @@
--- ISC License
---
--- Copyright © 2017, Simonas Kazlauskas
--- Copyright © 2018, Pedro Fanha
---
--- Permission to use, copy, modify, and/or distribute this software for any
--- purpose with or without fee is hereby granted, provided that the above1
--- copyright notice and this permission notice appear in all copies.
---
--- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
--- REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
--- AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
--- INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
--- LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
--- OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
--- PERFORMANCE OF THIS SOFTWARE.
-
 -- cpu, gpu, fan or hue
-local mode = 'hue'
+local mode = "hue"
 
 -- min and max speed of the CPU fan
-local fan = {700, 1800}
+local fan = {700, 2400}
 -- temperature min and max, applies for gpu and cpu
 local temp = {35, 65}
 
 -- max CPU temperature and color for alarm
 local cpu = {}
-cpu['max_temp'] = 60
-cpu['r'] = 0
-cpu['g'] = 0
-cpu['b'] = 255
+cpu["max_temp"] = 60
+cpu["r"] = 0
+cpu["g"] = 0
+cpu["b"] = 0xf
 
 -- max GPU temperature and color for alarm
 local gpu = {}
-gpu['max_temp'] = 70
-gpu['r'] = 0
-gpu['g'] = 255
-gpu['b'] = 0
+gpu["max_temp"] = 70
+gpu["r"] = 0
+gpu["g"] = 0xf
+gpu["b"] = 0
 
 -- max MB temperature and color for alarm
 local mb = {}
-mb['max_temp'] = 50
-mb['r'] = 255
-mb['g'] = 255
-mb['b'] = 255
+mb["max_temp"] = 50
+mb["r"] = 0xf
+mb["g"] = 0xf
+mb["b"] = 0xf
 
--- HSV color range
-local color_min = 0
-local color_max = 200
+--
+--
+--
+--
+--
+--
+--
+--
+--
+-- HSV color range (degrees)
+local hue_min = 0
+local hue_max = 200
 -- Steps for the "hue" mode
-local color_steps = 0.5
-local delay = 500
+local color_steps = 0.010
+local delay = 80 -- in milliseconds
 
 Lighting.SetStepDuration(511)
 Lighting.SetFlashingSpeed(0)
 Lighting.SetBreathingModeEnabled(false)
 
 -- https://gist.github.com/GigsD4X/8513963
-function HSVToRGB(hue, saturation, value)
+local function HSVToRGB(hue, saturation, value)
     -- Returns the RGB equivalent of the given HSV-defined color
     -- (adapted from some code found around the web)
 
     -- Get the hue sector
-    local hue_sector = math.floor( hue / 60 );
-    local hue_sector_offset = ( hue / 60 ) - hue_sector;
+    local hue_sector = math.floor(hue / 60)
+    local hue_sector_offset = (hue / 60) - hue_sector
 
-    local p = value * ( 1 - saturation );
-    local q = value * ( 1 - saturation * hue_sector_offset );
-    local t = value * ( 1 - saturation * ( 1 - hue_sector_offset ) );
+    local p = value * (1 - saturation)
+    local q = value * (1 - saturation * hue_sector_offset)
+    local t = value * (1 - saturation * (1 - hue_sector_offset))
 
     if hue_sector == 0 then
-        return value, t, p;
+        return value, t, p
     elseif hue_sector == 1 then
-        return q, value, p;
+        return q, value, p
     elseif hue_sector == 2 then
-        return p, value, t;
+        return p, value, t
     elseif hue_sector == 3 then
-        return p, q, value;
+        return p, q, value
     elseif hue_sector == 4 then
-        return t, p, value;
+        return t, p, value
     elseif hue_sector == 5 then
-        return value, p, q;
-    end;
-end;
+        return value, p, q
+    end
+end
 
 -- Blinks between red and configured hardware-color - the "alarm"
-function Alarm(r, g, b)
+local function Alarm(r, g, b)
     Lighting.BatchBegin()
     for i = 1, 8 do
-        Lighting.SetColour(i, 255, 0, 0)
+        Lighting.SetColour(i, 0xf, 0, 0)
     end
     Lighting.BatchEnd()
     os.sleep(200)
@@ -105,42 +97,44 @@ local max = 0
 if Aida64.IsInstalledAndRunning() then
     min = temp[1]
     max = temp[2]
-    if mode == 'cpu' then
-        hardware = 'TCPU'
-    elseif mode == 'gpu' then
-        hardware = 'TGPU1DIO'
+    if mode == "cpu" then
+        hardware = "TCPU"
+    elseif mode == "gpu" then
+        hardware = "TGPU1DIO"
         if not Aida64.GetSensorValue(hardware) then
-            print('GPU temparature is only available in paid version of AIDA64!')
-            print('Switching to hue mode...')
-            mode = 'hue'
+            print("GPU temparature is only available in paid version of AIDA64!")
+            print("Switching to hue mode...")
+            mode = "hue"
         end
-    elseif mode == 'fan' then
-        hardware = 'FCPU'
+    elseif mode == "fan" then
+        hardware = "FCPU"
         min = fan[1]
         max = fan[2]
     end
 else
-    print('AIDA64 is not running!')
-    print('Switching to hue mode...')
-    mode = 'hue'
+    if mode ~= "hue" then
+        print("AIDA64 is not running!")
+        print("Switching to hue mode...")
+        mode = "hue"
+    end
 end
 
 local color = 0
 local alarm = false
 while true do
     if Aida64.IsInstalledAndRunning() then
-        cpu['curr_temp'] = Aida64.GetSensorValue('TCPU')
-        mb['curr_temp'] = Aida64.GetSensorValue('TMOBO')
-        gpu['curr_temp'] = Aida64.GetSensorValue('TGPU1DIO')
+        cpu["curr_temp"] = Aida64.GetSensorValue("TCPU")
+        mb["curr_temp"] = Aida64.GetSensorValue("TMOBO")
+        gpu["curr_temp"] = Aida64.GetSensorValue("TGPU1DIO")
 
         -- temperature alarm
         local devices = {cpu, gpu, mb}
-        for devicecount=1,3 do
+        for devicecount = 1, 3 do
             local device = devices[devicecount]
             -- check if we really got the value (non-paid version for gpu temparature)
-            if device['curr_temp'] then
-                if device['curr_temp'] >= device['max_temp'] then
-                    Alarm(device['r'], device['g'], device['b'])
+            if device["curr_temp"] then
+                if device["curr_temp"] >= device["max_temp"] then
+                    Alarm(device["r"], device["g"], device["b"])
                     alarm = true
                 end
             end
@@ -148,14 +142,12 @@ while true do
     end
 
     if alarm == false then
-        if mode ~= 'hue' then
+        if mode ~= "hue" then
             if Aida64.IsInstalledAndRunning() then
                 local hardware_value = Aida64.GetSensorValue(hardware)
                 local percent = (math.max(hardware_value, min) - min) / (max - min)
-                color = color_max - percent * (color_max - color_min)
-                color = math.max(color, color_min)
-            else
-                print('AIDA64 is not running!')
+                color = hue_max - percent * (hue_max - hue_min)
+                color = math.max(color, hue_min)
             end
         else
             color = color + color_steps
@@ -164,7 +156,7 @@ while true do
             end
         end
 
-        -- finally, set the color; for all modes
+        -- finally, set the color for all modes
         local r, g, b = HSVToRGB(color, 1.0, 1.0)
         r = tonumber(("%x"):format(r * 15), 16)
         g = tonumber(("%x"):format(g * 15), 16)
