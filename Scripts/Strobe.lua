b2adaa3bed9e90f7e1cc4764f784e5a1fba08dd6
Pedro Fanha <ixjf@outlook.com>
2019-04-18 14:05:08 +0100

    Move 'delay' local var out of for loop (Strobe effect)

--- Scripts/Strobe.lua
+++ Scripts/Strobe.lua
@@ -11,9 +11,9 @@ local function flash(speed, time)
     -- speed: flashes/sec
     -- time: seconds
     local num_flashes = speed * time
+    local delay = (time * 1000) / (num_flashes * 2)
 
     for i = 1, num_flashes do
-        local delay = (time * 1000) / (num_flashes * 2)
         Lighting.SetAllEnabled(true)
         os.sleep(delay)
         Lighting.SetAllEnabled(false)
