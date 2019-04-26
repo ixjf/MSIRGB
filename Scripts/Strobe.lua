36e560b0520f4f3781d67b75dd196022e0d33276
Pedro Fanha <ixjf@outlook.com>
2019-04-26 20:12:52 +0100

    Rename '-- Variables' comment to '-- User-changeable variables'

--- Scripts/Strobe.lua
+++ Scripts/Strobe.lua
@@ -1,6 +1,6 @@
 -- Based on Tweaking4Fun's Strobe effect
 
--- Variables
+-- User-changeable variables
 local flashes_per_second = 20
 local pulse_time = 0.6 -- how long each flashing pulse will take, in seconds (repeat flashes_per_second flashes pulse_time times)
 local pulse_delay = 1000 -- how long between each flashing pulse, in milliseconds
